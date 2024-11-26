3b7cd07118c68dd22af16046e5193eb61e39cd2f
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-11-26 19:56:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+</script>
+
+<template>
+  <div>
+    <HomeView/>
+  </div>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+console.log("hola")
+
+</script>
+
+<template>
+
+    <div>
+        <p>hola</p>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
